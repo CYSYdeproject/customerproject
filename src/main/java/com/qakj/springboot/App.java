4257c8cdde86d4eadab3c300e63844b6f608d135
jerry <jerry@aaa.com>
2020-06-03 15:26:46 +0800

    aaaaa

--- src/main/java/com/qakj/springboot/App.java
+++ src/main/java/com/qakj/springboot/App.java
@@ -10,6 +10,6 @@ public class App {
 		SpringApplication.run(App.class, args);
 		System.out.println("ooooooooooooo");
 		System.out.println("hello everybody");
-	
+	System.out.println("nice to meet you!!!");
 	}
 }
