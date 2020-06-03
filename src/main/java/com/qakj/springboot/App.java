b1ba3f26013e973aa684ffbab26ea3c07b42ddaa
tom <tom@123.com>
2020-06-03 15:24:37 +0800

    fdsgrsg

--- src/main/java/com/qakj/springboot/App.java
+++ src/main/java/com/qakj/springboot/App.java
@@ -9,6 +9,7 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 		System.out.println("ooooooooooooo");
+		System.out.println("hello everybody");
 	
 	}
 }
