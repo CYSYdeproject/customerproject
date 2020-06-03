ad89fa0965c964c9c3d247c595d629f3c7177e47
tom <tom@aaa.com>
2020-06-03 16:46:36 +0800

    hello

--- src/main/java/com/qakj/springboot/App.java
+++ src/main/java/com/qakj/springboot/App.java
@@ -10,11 +10,11 @@ public class App {
 		SpringApplication.run(App.class, args);
 		System.out.println("ooooooooooooo");
 		System.out.println("hello everybody");
-	System.out.println("nice to  futfrt meet you!!!1111");
+	    System.out.println("nice to  futfrt meet you!!!1111");
 
-	System.out.println("sldjfh---------");
+	    System.out.println("sldjfh---------");
 
-	System.out.println("lalalalalalal");
+	    System.out.println("lalalalalalal");
 
 	}
 }
